3e1e68bf697dd51e126416607432f8edfcb0ffed
Jason Rudin <j.b.rudin@gmail.com>
2022-01-05 23:35:33 -0500

    Updated snake into snake and snake segment classes. Need to work on appending function, not working correctly yet.

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <canvas className="border-2"></canvas>
+      <p>Score: {score}</p>
       {setupCanvas()}
       {run()}
     </div>
@@ -25,8 +26,7 @@ var snake = undefined;
 var food = undefined;
 var segmentSize = 10;
 var speed = 2;
-var dx = speed;
-var dy = 0;
+var score = 0;
 
 function run() {
   if (typeof window !== "undefined") {
@@ -47,37 +47,39 @@ function setupCanvas() {
     snake = new Snake(canvas.width / 2, canvas.height / 2);
     food = new Food();
 
-    window.addEventListener('keydown', function(event){
-      switch(event.code){
+    window.addEventListener('keydown', function (event) {
+      switch (event.code) {
         case "KeyA":
         case "ArrowLeft":
-          dx = -speed;
-          dy = 0;
+          snake.head.dx = -speed;
+          snake.head.dy = 0;
           break;
-          case "KeyD":
+        case "KeyD":
         case "ArrowRight":
-          dx = speed;
-          dy = 0;
+          snake.head.dx = speed;
+          snake.head.dy = 0;
           break;
-          case "KeyW":
+        case "KeyW":
         case "ArrowUp":
-          dy = -speed;
-          dx = 0;
+          snake.head.dy = -speed;
+          snake.head.dx = 0;
           break;
-          case "KeyS":
+        case "KeyS":
         case "ArrowDown":
-          dx = 0;
-          dy = speed;
+          snake.head.dx = 0;
+          snake.head.dy = speed;
           break;
       }
     });
   }
 }
 
-//Snake Class
-function Snake(startX, startY) {
-  this.x = startX;
-  this.y = startY;
+//Segment Class
+function SnakeSegment(x, y, dx, dy) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
 
   this.xForDrawing = this.x - segmentSize / 2;
   this.yForDrawing = this.y - segmentSize / 2;
@@ -85,13 +87,12 @@ function Snake(startX, startY) {
   this.draw = function () {
     c.beginPath();
     c.rect(this.xLeftBound, this.yTopBound, segmentSize, segmentSize);
-    c.fillStyle = 'black';
     c.fill();
   }
 
-  this.update = function () {
-    this.x += dx;
-    this.y += dy;
+  this.update = function (x, y) {
+    this.x = x;
+    this.y = y;
     this.xLeftBound = this.x - segmentSize / 2;
     this.xRightBound = this.x + segmentSize / 2;
     this.yTopBound = this.y - segmentSize / 2;
@@ -100,27 +101,88 @@ function Snake(startX, startY) {
     this.draw();
   }
 
-  this.checkWallCollision = function(){
-    if(this.xLeftBound <= 0 || this.xRightBound >= canvas.width){
-      dx = 0;
+  this.checkWallCollision = function () {
+    if (this.xLeftBound <= 0 || this.xRightBound >= canvas.width) {
+      this.dx = 0;
     }
-    if(this.yTopBound <= 0 || this.yBottomBound >= canvas.height){
-      dy = 0;
+    if (this.yTopBound <= 0 || this.yBottomBound >= canvas.height) {
+      this.dy = 0;
     }
   }
 }
 
-//Food Class
-function Food(){
-  this.x = Math.random()*canvas.width;
-  this.y = Math.random()*canvas.height;
+//Snake Class
+function Snake(startX, startY) {
+  this.segmentArray = [];
 
-  this.draw = function(){
+  //create snake head
+  var s = new SnakeSegment(startX, startY, speed, 0);
+  this.segmentArray.push(s);
+  this.head = this.segmentArray[0];
+
+  this.update = function () {
+    //iterate through all pieces of the snake
+    for (var i = 0; i < this.segmentArray.length; i++) {
+      if(i === 0){
+        c.fillStyle = 'black';
+        this.segmentArray[i].update(this.segmentArray[i].x + this.segmentArray[i].dx, 
+          this.segmentArray[i].y + this.segmentArray[i].dy);
+        
+      }
+      else{
+        c.fillStyle = 'blue';
+        console.log(`head x: ${this.segmentArray[1]}, tail x: ${this.segmentArray[0]}`);
+        this.segmentArray[i].update(this.segmentArray[i-1].x, this.segmentArray[i-1].y);
+      }
+    }
+  }
+
+  //Add another segment to the snake
+  this.append = function (x, y) {
+    //Find the direction of the head
+    var dXCoefficient = 0;
+    var dYCoefficient = 0;
+
+    if(this.head.dx < 0){
+      dXCoefficient = -1;
+    }
+    if(this.head.dx > 0){
+      dXCoefficient = 1;
+    }
+    if(this.head.dy < 0){
+      dYCoefficient = -1;
+    }
+    if(this.head.dy > 0){
+      dYCoefficient = 1;
+    }
+
+    //Get the butt of the snake
+    var backSegment = this.segmentArray[this.segmentArray.length-1];
+
+    //Get new positions, adjusted by the direction and offset by segment size
+    var newX = backSegment.x - (segmentSize * dXCoefficient);
+    var newY = backSegment.y - (segmentSize * dYCoefficient);
+
+    console.log(`back segment y: ${backSegment.y}, new segment y:${newY}`);
+
+    //add new segment to the snake
+    var newSegment = new SnakeSegment(newX, newY, this.head.dx, this.head.dy);
+    this.segmentArray.push(newSegment);
+  }
+}
+
+//Food Class
+function Food() {
+  this.x = Math.random() * canvas.width;
+  this.y = Math.random() * canvas.height;
+  c.fillStyle = '#ff6150';
+
+  this.draw = function () {
     c.beginPath();
-    c.arc(this.x, this.y, 5, 0, Math.PI*2, false);
-    c.fillStyle = '#ff6150';
+    c.arc(this.x, this.y, 5, 0, Math.PI * 2, false);
     c.fill();
   }
+
 }
 
 function animate() {
@@ -128,6 +190,21 @@ function animate() {
   c.clearRect(0, 0, canvas.width, canvas.height);
   snake.update();
   food.draw();
+  if (isFoodEaten(snake.segmentArray[0], food)) {
+    snake.append(food.x, food.y);
+    food.x = Math.random() * canvas.width;
+    food.y = Math.random() * canvas.height;
+    score++;
+  }
 }
+
+function isFoodEaten(s, f) {
+  var xDist = (s.x - f.x);
+  var yDist = (s.y - f.y);
+  if (Math.abs(xDist) < segmentSize && Math.abs(yDist) < segmentSize) {
+    return true;
+  }
+}
+
 
 
